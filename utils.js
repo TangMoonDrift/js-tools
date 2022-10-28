293bde8feddb445fa5cab7d979f60b565645abc8
夏夜行船 <MoonDrift@163.com>
2022-10-28 20:47:09 +0800

    feat: init

--- utils.js
+++ utils.js
@@ -1,140 +1,104 @@
-let tools = {
-    //对象深度克隆
-    deepClone: function (target, origin) {
-        var tar = target || {};
-        for (var prop in origin) {
-            if (origin.hasOwnProperty(prop)) {
-                if (Object.prototype.toString.call(origin[prop]) === '[object Object]' && typeof origin[prop] !== null) {
-                    tar[prop] = {};
-                    deepClone(tar[prop], origin[prop]);
-                } else if (Object.prototype.toString.call(origin[prop]) === '[object Array]') {
-                    tar[prop] = [];
-                    deepClone(tar[prop], origin[prop]);
-                } else {
-                    tar[prop] = origin[prop];
-                }
-            }
-        }
-        return tar;
-    },
-
-    //类型判断
-    _typeof: function (target) {
-
-    },
-
-    //数组去重
-    uniqueArray: function (target) {
-        if (Object.prototype.toString.call(target) !== '[object Array]') {
-            alert('请输入数组');
-            return false;
-        }
-        var tempObj = {},
-            newArr = [],
-            len = target.length;
-        for (var i = 0; i < len; i++) {
-            if (!tempObj[target[i]]) {
-                tempObj[target[i]] = i + '';
-                newArr.push(target[i]);
-            }
-        }
-        return newArr;
-    },
-
-    //数组冒泡排序
-    bubbleSort: function (arr, flag) {
-        var arr = Object.prototype.toString.call(arr) == '[object Array]' ? arr : [];
-        var len = arr.length;
-        for (let i = 0; i < len - 1; i++) {
-            for (let j = 0; j < len - i - 1; j++) {
-                if (flag == true && arr[j + 1] < arr[j]) {
-                    var temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }else if(flag == false && arr[j + 1] > arr[j]) {
-                    var temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        return arr;
-    },
-
-    //正则exec
-    regExec: function (str, reg) {
-        var str = str,
-            reg = reg;
-        var result = reg.exec(str);
-        return result;
-    },
-
-    //拖拽函数
-    drag: function (ele) {
-        ele.addEventListener('mousedown', function(e) {
-            var x = e.pageX - this.offsetLeft;
-            var y = e.pageY - this.offsetTop;
-            document.addEventListener('mousemove', move, false);
-
-            function move(e) {
-                ele.style.left = e.pageX - x + 'px';
-                ele.style.top = e.pageY - y + 'px';
-            }
-            this.addEventListener('mouseup', function() {
-                document.removeEventListener('mousemove', move, false);
-            }, false);
-        }, false);
-    },
-
-    //异步加载
-    async: function (url, callback) {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        if (script.readyState) {
-            script.onreadystatechange = function() {
-                if (script.readyState == 'complete' || script.readyState == 'loaded') {
-                    callback();
-                } else {
-                    script.onload = function() {
-                        callback();
-                    }
-                }
-            }
-        };
-        script.src = url;
-        document.head.appendChild(script);
-    },
-
-    //类名选择
-    byClassName: function () {
-        if (document.getElementsByClassName) {
-            Element.prototype.getElementsByClassName = document.getElementsByClassName;
-            Document.prototype.getElementsByClassName = document.getElementsByClassName;
-        } else {
-            Element.prototype.getElementsByClassName = Document.prototype.getElementsByClassName = function(_className) {
-                var domArray = document.getElementsByTagName('*');
-                var len = domArray.length;
-                for (var i = 0; i < len; i++) {
-
-                }
-            }
-        }
-    },
-
-    //设计模式
-    //单例化方法
-    GetSingleton: function (fn) {
-        var result;
-        return function() {
-            if (result != null) {
-                return result;
-            }
-            result = fn.apply(this, arguments);
-            return result;
-        };
-    },
-    //验证控件
-    Validator: function () {
-
+const deepClone = (origin) => {
+  // 判断类型 基本判断
+  if (origin === null) return null
+  if (typeof origin !== 'object') return origin
+  // 使用缓存解决循环引用
+  if (!deepClone.cache) {
+    deepClone.cache = new WeakMap()
+  }
+  if (deepClone.cache.has(origin)) {
+    return deepClone.cache.get(origin)
+  }
+  // 引用数据类型 set map
+  if (origin instanceof Set) {
+    const set = new Set()
+    deepClone.cache.set(origin, set)
+    origin.forEach(item => {
+      set.add(deepClone(item))
+    })
+    return set
+  } else if (origin instanceof Map) {
+    const map = new Map()
+    deepClone.cache.set(origin, map)
+    origin.forEach((value, key) => {
+      map.set(key, deepClone(value))
+    })
+    return map
+  } else if (origin instanceof RegExp) {
+    return new RegExp(origin)
+  } else {
+    const result = new origin.constructor()
+    deepClone.cache.set(origin, result)
+    for (const originKey in origin) {
+      result[originKey] = deepClone(origin[originKey])
     }
-};
+    return result
+  }
+}
+
+const drag = (ele) => {
+  ele.addEventListener('mousedown', function (e) {
+    let x = e.pageX - this.offsetLeft
+    let y = e.pageY - this.offsetTop
+    document.addEventListener('mousemove', move, false)
+
+    function move(e) {
+      ele.style.left = e.pageX - x + 'px'
+      ele.style.top = e.pageY - y + 'px'
+    }
+
+    this.addEventListener('mouseup', function () {
+      document.removeEventListener('mousemove', move, false)
+    }, false)
+  }, false)
+}
+
+const _async = (url, callback) => {
+  let script = document.createElement('script')
+  script.type = 'text/javascript'
+  if (script.readyState) {
+    script.onreadystatechange = function () {
+      if (script.readyState === 'complete' || script.readyState === 'loaded') {
+        callback()
+      } else {
+        script.onload = function () {
+          callback()
+        }
+      }
+    }
+  }
+  script.src = url
+  document.head.appendChild(script)
+}
+
+const singleton = (fn) => {
+  let result
+  return function () {
+    if (result != null) {
+      return result
+    }
+    result = fn.apply(this, arguments)
+    return result
+  }
+}
+
+const _instanceof = (obj, fn) => {
+  let proto = Object.getPrototypeOf(obj)
+  if (!proto) return false
+  if (proto === fn.prototype) {
+    return true
+  } else {
+    return _instanceof(proto, fn)
+  }
+}
+
+const _new = (fn, ...args) => {
+  if (!fn ||
+    typeof fn !== 'function' ||
+    typeof fn !== 'object'
+  ) return Object.create(null)
+  const instance = Object.create(fn.prototype)
+  const res = fn.call(instance, ...args)
+  return typeof res === 'function' || typeof res === 'object' ? res : instance
+}
+
